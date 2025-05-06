8701cf3f6f69eb1c1a0d31ad788aa96595052b66
Raff Viglianti <raffaeleviglianti@gmail.com>
2025-05-06 15:31:53 -0400

    metadata fix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,11 +7,8 @@ const config: GatsbyConfig = {
   pathPrefix: basePath,
   siteMetadata: {
     title: `scd`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://database.radiopreservation.org/`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-postcss",
